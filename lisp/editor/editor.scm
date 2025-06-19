a9cd09e923c1da7fbc2c809c891a6faa773eef58
Preston <dev@spreadsheet.institute>
2025-06-19 08:17:40 +0000

    Adding buffer to capture keystrokes

--- lisp/editor/editor.scm
+++ lisp/editor/editor.scm
@@ -21,9 +21,19 @@
         element))
 
 (define
+    (<div:class:id:innerHTML> classifier identifier content)
+    (let ((element (<div>)))
+        (element.classList.add classifier)
+        (set! element.id identifier)
+        (set! element.innerHTML content)
+        element))
+
+(define
     editor
     (document.getElementById "editor"))
 
 (editor.appendChild (<input:id:placeholder> "cli" "Command Line"))
+(editor.appendChild (<input:id:placeholder> "buffer" "Buffer"))
+
 (editor.appendChild (<div:id:innerHTML> "palette" "Command Palette"))
 (editor.appendChild (<div:id:innerHTML> "grid" "Definition Editor"))
