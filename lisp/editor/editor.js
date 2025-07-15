13b4ea56b8266ffdf4ef7dab359b325ab5a3d36d
Preston <dev@spreadsheet.institute>
2025-07-15 05:40:41 +0000

    Moving save keybinding from a global event listener to a Quill.js binding at initialization

--- lisp/editor/editor.js
+++ lisp/editor/editor.js
@@ -1,36 +1,3 @@
-// Borrowing wren's syntax highlighter
-hljs.configure({
-    languages: ["wren"]
-});
-
-// Quill.js editor initialization options
-const options = {
-    modules: {
-        toolbar: false,
-        syntax: true
-    },
-    theme: "snow"
-};
-
-// Initial editor setup
-const quill = new Quill("#editor", options);
-quill.setText("DEFINE(\n\tIDENTITY,\n\tLAMBDA(\n\t\tx,\n\t\tx))");
-quill.formatText(0, quill.getLength(), "size", "30px");
-quill.focus();
-quill.setSelection(quill.getLength()-3);
-
-// Add "Control/Command + S" keyboard event override
-document.addEventListener("Keydown", (event) => {
-    const control_or_command_is_down = event.ctrlKey || event.metaKey;
-    const shift_is_down = event.shiftKey;
-
-    if (control_or_command_is_down && shift_is_down && event.key === "s") {
-        event.preventDefault();
-        console.log("Saving definition...");
-        definition_download();
-    }
-});
-
 // Add editor-to-plaintext file content convertor
 let definition_download = () => {
     const definition_textline = quill.getText().replace(/(\r\n|\n|\r|\t)/gm, " ").trim();
@@ -48,5 +15,38 @@ let definition_download = () => {
     
     URL.revokeObjectURL(url);
 }
+
+// Borrowing wren's syntax highlighter
+hljs.configure({
+    languages: ["wren"]
+});
+
+const bindings = {
+    saveDefinition: {
+        key: "S",
+        shiftKey: true,
+        ctrlKey: true,
+        handler: definition_download
+    }
+}
+
+// Quill.js editor initialization options
+const options = {
+    modules: {
+        toolbar: false,
+        syntax: true,
+        keyboard: {
+            bindings: bindings
+        }
+    },
+    theme: "snow"
+};
+
+// Initial editor setup
+const quill = new Quill("#editor", options);
+quill.setText("DEFINE(\n\tIDENTITY,\n\tLAMBDA(\n\t\tx,\n\t\tx))");
+quill.formatText(0, quill.getLength(), "size", "30px");
+quill.focus();
+quill.setSelection(quill.getLength()-3);
 
 // const Delta = Quill.import("delta");
