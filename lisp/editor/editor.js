8a31f59ca3276ccfe66f2af868f10019ef18956d
Preston <dev@spreadsheet.institute>
2025-07-15 04:17:13 +0000

    Unselecting function body upon editor initialization

--- lisp/editor/editor.js
+++ lisp/editor/editor.js
@@ -17,6 +17,6 @@ const quill = new Quill("#editor", options);
 quill.setText("DEFINE(\n\tIDENTITY,\n\tLAMBDA(\n\t\tx,\n\t\tx))");
 quill.formatText(0, quill.getLength(), "size", "30px");
 quill.focus();
-quill.setSelection(quill.getLength()-4, 1);
+quill.setSelection(quill.getLength()-3);
 
 // const Delta = Quill.import("delta");
