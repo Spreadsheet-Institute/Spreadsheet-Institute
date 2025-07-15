0cc2729ba3b995caa3f970c5db80bd2f09fee35f
Preston <dev@spreadsheet.institute>
2025-07-15 05:03:28 +0000

    Adding definition download link and keyboard event listener

--- lisp/editor/editor.js
+++ lisp/editor/editor.js
@@ -19,4 +19,31 @@ quill.formatText(0, quill.getLength(), "size", "30px");
 quill.focus();
 quill.setSelection(quill.getLength()-3);
 
+// Add "Control/Command + S" keyboard event override
+document.addEventListener("Keydown", (event) => {
+    const control_command_is_down = event.ctrlKey || event.metaKey;
+
+    if (control_command_is_down && event.key.toLowerCase() === "s") {
+        definition_download();
+    }
+});
+
+// Add editor-to-plaintext file content convertor
+let definition_download = () => {
+    const definition_textline = quill.getText().replace(/(\r\n|\n|\r|\t)/gm, " ").trim();
+    const blob = new Blob([definition_textline], { type: "text/plain;charset=utf-8" });
+    const url = URL.createObjectURL(blob);
+    const downloadLink = document.createElement("a");
+    
+    downloadLink.href = url;
+    downloadLink.download = "spreadsheet.lisp";
+    downloadLink.style.display = "none";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+    
+    URL.revokeObjectURL(url);
+}
+
 // const Delta = Quill.import("delta");
