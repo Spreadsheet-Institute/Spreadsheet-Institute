9cc598e9f26afe326e5649426de77d29a44b1862
Preston <dev@spreadsheet.institute>
2025-07-31 02:27:02 +0000

    Miscellaneous desiderata

--- latin/latin.scm
+++ latin/latin.scm
@@ -1,3 +1,13 @@
 (set! self.greet (lambda () "Salve Mundi"))
+; (console.log (greet))
 
-(console.log (greet))
+(define
+    (input-textarea-element)
+    (document.getElementById "input-textarea"))
+
+(define
+    (input-text)
+    (let ((input-textarea (input-textarea-element)))
+        (input-textarea.value)))
+
+(console.log (input-text))
