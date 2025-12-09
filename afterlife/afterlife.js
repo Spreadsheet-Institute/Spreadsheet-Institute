af2e2f8912dca61d015c8d497a66896630698221
Preston <dev@spreadsheet.institute>
2025-12-09 09:54:51 +0000

    Miscellaneous desiderata

--- afterlife/afterlife.js
+++ afterlife/afterlife.js
@@ -1,7 +1,7 @@
 // Submit query to Afterlife API when [Enter] key is pressed
 let submit_query = (event) => {
     if (event.keyCode === 13) {
-        console.log(event.text);
+        console.log(event.innerHTML);
     }
 }
 
