956a0df8066205dbec7178304066bff30cb49ebe
Preston <dev@spreadsheet.institute>
2025-12-09 09:47:00 +0000

    init commit

---
+++ afterlife/afterlife.js
@@ -0,0 +1,8 @@
+// Submit query to Afterlife API when [Enter] key is pressed
+let submit_query = (event) => {
+    if (event.keyCode === 13) {
+        console.log(event.text);
+    }
+}
+
+document.getElementById("input").addEventListener("keydown", submit_query);
